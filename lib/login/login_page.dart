61775e41392a6c606011966f98c509a3b7035b86
klsh1tt <klsh1tt@gmail.com>
2022-01-05 18:18:54 +0900

    ログイン画面へのボタン作成/google-servicies.jsonの編集 課題：actionsがundefinedになりコンパイルできない

---
+++ lib/login/login_page.dart
@@ -0,0 +1,71 @@
+import 'package:book_list_sample/login/login_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<LoginModel>(
+      create: (_) => LoginModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'ログイン',
+          ),
+        ),
+        body: Center(
+          child: Consumer<LoginModel>(builder: (context, model, child) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: model.titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                    onChanged: (text) {
+                      model.setEmail(text);
+                    },
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    controller: model.authorController,
+                    decoration: InputDecoration(
+                      hintText: 'パスワード',
+                    ),
+                    onChanged: (text) {
+                      model.setPassword(text);
+                    },
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      //追加の処理
+                      try {
+                        await model.signUp();
+                        //Navigator.of(context).pop(model.title);
+                      } catch (e) {
+                        //エラー時、SnackBarを表示
+                        final snackBar = SnackBar(
+                          backgroundColor: Colors.red,
+                          content: Text(e.toString()),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                    child: Text('ログイン'),
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register/register_model.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class RegisterModel extends ChangeNotifier {
+  final titleController = TextEditingController();
+  final authorController = TextEditingController();
+
+  String? email;
+  String? password;
+
+  void setEmail(String email) {
+    this.email = email;
+    notifyListeners();
+  }
+
+  void setPassword(String password) {
+    this.password = password;
+    notifyListeners();
+  }
+
+  Future signUp() async {
+    this.email = titleController.text;
+    this.password = authorController.text;
+
+    // firebase authでユーザ作成
+
+    // firestoreに追加
+
+    // await FirebaseFirestore.instance.collection('books').doc(book.id).update({
+    //   'title': email,
+    //   'author': password,
+    // });
+  }
+}
